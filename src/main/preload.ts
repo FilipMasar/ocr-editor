87b4dc668aa7835a08687b2055530a70695a5f96
Filip Masar <filip.masar99@gmail.com>
2023-02-12 18:27:38 +0000

    init

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,27 +1,2 @@
-import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
-
-export type Channels = 'project-channel' | 'config-channel' | 'editor-channel';
-export type ChannelData = {
-  action: string;
-  payload?: any;
-};
-
-contextBridge.exposeInMainWorld('electron', {
-  ipcRenderer: {
-    sendMessage(channel: Channels, data: ChannelData) {
-      ipcRenderer.send(channel, data);
-    },
-    on(channel: Channels, func: (data: ChannelData) => void) {
-      const subscription = (_event: IpcRendererEvent, data: ChannelData) =>
-        func(data);
-      ipcRenderer.on(channel, subscription);
-
-      return () => {
-        ipcRenderer.removeListener(channel, subscription);
-      };
-    },
-    once(channel: Channels, func: (data: ChannelData) => void) {
-      ipcRenderer.once(channel, (_event, data) => func(data));
-    },
-  },
-});
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,20 +1,7 @@
-import { MantineProvider } from '@mantine/core';
-import AppRoutes from './AppRoutes';
-import ProjectProvider from './context/ProjectContext';
-import SettingsProvider from './context/SettingsContext';
+
 
 export default function App() {
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{ colorScheme: 'light' }}
-    >
-      <ProjectProvider>
-        <SettingsProvider>
-          <AppRoutes />
-        </SettingsProvider>
-      </ProjectProvider>
-    </MantineProvider>
+    <p>AHOJs</p>
   );
 }
