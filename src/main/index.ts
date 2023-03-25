493b1d9a237a010dfdb2bf8aec05749baef4e5e1
Filip Masar <filip.masar99@gmail.com>
2023-03-25 17:25:05 +0000

    open dev tools only when run locally

--- src/main/index.ts
+++ src/main/index.ts
@@ -59,7 +59,8 @@ const createWindow = async (): Promise<void> => {
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  if (process.env.NODE_ENV === 'development') 
+    mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
